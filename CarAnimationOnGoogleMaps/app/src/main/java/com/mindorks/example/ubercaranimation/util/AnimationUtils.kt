75b7b481bd3f23796773f3db7173a1cafdc19b4d
mossssama <mo99saleh@gmail.com>
2023-04-28 05:26:00 +0300

    Tracking the user location with a Car on the Map

---
+++ CarAnimationOnGoogleMaps/app/src/main/java/com/mindorks/example/ubercaranimation/util/AnimationUtils.kt
@@ -0,0 +1,24 @@
+package com.mindorks.example.ubercaranimation.util
+
+import android.animation.ValueAnimator
+import android.view.animation.LinearInterpolator
+
+object AnimationUtils {
+
+    /* M Osama: animator for blackLine over greyLine */
+    fun polylineAnimator(): ValueAnimator {
+        val valueAnimator = ValueAnimator.ofInt(0, 100)
+        valueAnimator.interpolator = LinearInterpolator()
+        valueAnimator.duration = 4000
+        return valueAnimator
+    }
+
+    /* M Osama: animator for car movement */
+    fun carAnimator(): ValueAnimator {
+        val valueAnimator = ValueAnimator.ofFloat(0f, 1f)
+        valueAnimator.duration = 3000
+        valueAnimator.interpolator = LinearInterpolator()
+        return valueAnimator
+    }
+
+}
